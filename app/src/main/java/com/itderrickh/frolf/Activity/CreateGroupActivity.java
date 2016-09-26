0a881b714111f9ba4314c4f67366867198d1fa7e
itderrickh <heined50@uwosh.edu>
2016-09-26 09:20:17 -0500

    Added create group page.
    Added create group page stub. Re-created the menu as well.

---
+++ app/src/main/java/com/itderrickh/frolf/Activity/CreateGroupActivity.java
@@ -0,0 +1,28 @@
+package com.itderrickh.frolf.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.itderrickh.frolf.R;
+
+public class CreateGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_group);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
--- app/src/main/java/com/itderrickh/frolf/Activity/MainActivity.java
+++ app/src/main/java/com/itderrickh/frolf/Activity/MainActivity.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -84,14 +82,14 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
+        if (id == R.id.nav_creategroup) {
+            Intent createGroup = new Intent(getApplicationContext(), CreateGroupActivity.class);
+            startActivity(createGroup);
         } else if (id == R.id.nav_logout) {
             SharedPreferences preferences = getSharedPreferences("FROLF_SETTINGS", Context.MODE_PRIVATE);
             preferences.edit().remove("Auth_Token").remove("Email").commit();
